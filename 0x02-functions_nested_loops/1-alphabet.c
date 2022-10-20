6a63c38c76352f8cd36df766b167369254b0ca61
Emmanuel <olarewajuemmanuel4@gmail.com>
2022-10-19 19:22:47 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - print a line of code
+*
+* Return: 0
+*/
+
+int main(void)
+
+{
+
+char I;
+
+for (I = 'a'; I <= 'z'; I++)
+
+{
+printf("%d\n", I);
+
+}
+
+return (0);
+}
