ac32716d480b3d4c24a14bbbcc32a7c7fe7305c1
Emmanuel <olarewajuemmanuel4@gmail.com>
2022-10-20 16:33:07 -0700

    1-alphabe.c

---
+++ 0x02-functions_nested_loops/1-alphabe.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* main - printing a-z in lowercase
+* written by atom
+* Return: 0
+*/
+
+
+void print_alphabet(void);
+
+{
+
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+
+
+
+_putchar(letter);
+
+_putchar('\n');
+
+
+return (0);
+
+}
