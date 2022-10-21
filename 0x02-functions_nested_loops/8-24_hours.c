319317c9a6f7d61b03d90e99075d903e23406ad6
Emmanuel <olarewajuemmanuel4@gmail.com>
2022-10-21 14:52:51 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,8 +19,8 @@ for (minutes = 0; minutes < 60; minutes++)
 
 {
 
-_putchar(hours / 10 + '0');
-_putchar(hours % 10 + '0');
+_putchar(hour / 10 + '0');
+_putchar(hour % 10 + '0');
 _putchar(':');
 _putchar(minutes / 10 + '0');
 _putchar(minutes % 10 + '0');
