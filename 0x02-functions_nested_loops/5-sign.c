f3928cf4f5ad06787f1eceb6f01c2efcd5e77798
Emmanuel <olarewajuemmanuel4@gmail.com>
2022-10-21 05:20:45 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,20 +13,20 @@ int print_sign(int n)
 
 if (n > 0)
 {
-_pitchar("+");
+_putchar('+');
 return (1);
 }
 
-else if (n < 0)
+else if (n == 0)
 {
-_putchar("-");
-return (-1);
+_putchar('0');
+return (0);
 }
 
-else (n == 0)
+else
 {
-_putchar("0")
-return (0);
+_putchar('-')
+return (-1);
 }
 
 }
