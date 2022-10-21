5905bc105455e7b204c69eafd722fc9bfc65a722
Emmanuel <olarewajuemmanuel4@gmail.com>
2022-10-21 03:46:42 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,30 @@
+#Include "main.h"
+
+/**
+* _isalpha - check if a character is lowercase
+* Written by atom
+* @c: the character
+* Return: 1 if c is alpahabet, 0 if not
+*/
+
+int _isalpha(int c)
+
+{
+
+if (c >= 'a' && c <= 'z')
+
+return (1);
+
+else
+
+return (0);
+
+if (c >= 'A' && c <= 'Z')
+
+return (1);
+
+else
+
+return (0);
+
+}
