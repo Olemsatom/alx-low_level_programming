4f3fdf2dc7315df69b42981404acc3cd90240742
Emmanuel <olarewajuemmanuel4@gmail.com>
2022-10-20 17:23:26 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_alphabet_x10  - printing a-z (lowercase) in 10 places
+* print_alphabet_x10 - printing a-z (lowercase) in 10 places
 * written by atom
 * Return: 0
 */
@@ -11,19 +11,15 @@ void print_alphabet_x10(void)
 
 {
 
-Int count = 0;
+int count = 0;
 char letter;
 
-While (count++ <= 9)
+while (count++ <= 9)
 
 for (letter = 'a'; letter <= 'z'; letter++)
-
-
 
 _putchar(letter);
 
 _putchar('\n');
-
-
 
 }
