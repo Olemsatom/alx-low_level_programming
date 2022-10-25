c197bfa988c2fb98679ccf24a9576edbe4ffd19f
Emmanuel <olarewajuemmanuel4@gmail.com>
2022-10-25 02:46:43 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* add - function that adds two integers
+* a = first integer, b = second integer, d = answer
+* Return: value of the addition
+*/
+
+int add(int, int)
+
+{
+
+int a;
+int b;
+int d;
+
+a + b = d
+
+_putchar('%d\n', d)
+
+return (0)
+
+}
