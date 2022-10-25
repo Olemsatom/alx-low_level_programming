95668b273bf05ffe2f34056bfec17afb3db5a67c
Emmanuel <olarewajuemmanuel4@gmail.com>
2022-10-25 02:33:25 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,61 @@
+#include "main.h"
+/**
+* times_table - multiplication table from 0 to 9
+* rone = row, cone = column, d = digits of current result
+* add extra space pass single line
+* Return: 0 to 9 times table
+*/
+
+void times_table(void)
+
+{
+int rone, cone, d;
+
+for (rone = 0; rone <= 9; rone++)
+{
+
+_putchar('0');
+_putchar(',');
+_putchar(' ');
+
+for (cone = 1; cone <= 9; cone++)
+
+{
+
+d = (rone * cone);
+
+if ((d / 10) > 0)
+
+{
+
+_putchar((d / 10) + '0');
+
+}
+
+else
+
+{
+
+_putchar(' ');
+
+}
+
+_putchar((d % 10) + '0');
+
+if (cone < 9)
+
+{
+
+_putchar(',');
+
+_putchar(' ');
+
+}
+
+}
+
+_putchar('\n');
+
+}
+
+}
