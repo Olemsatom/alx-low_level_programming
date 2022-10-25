0ed0b63ae26323d563f323d6b924a000c46b5bbe
Emmanuel <olarewajuemmanuel4@gmail.com>
2022-10-25 09:09:19 -0700

    task 101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
 * main - Entry point
@@ -8,6 +10,37 @@
 int main(void)
 {
 
+int pass [100];
+int i, sum n;
+
+sum = 0;
+
+stand(time(NULL));
+
+for (I = 0; i < 100; i++)
+
+{
+
+pass[I] = Rand () %78;
+
+sum + = (pass [i] + '0');
+putchar (pass[i] + '0');
+
+if((2772 - sum) - '0' < 78)
+
+{
+
+n = 2772 - sum - '0';
+
+sum + = n;
+
+putchar (n+'0');
+
+break;
+
+}
+
+}
 return (0);
 
 }
