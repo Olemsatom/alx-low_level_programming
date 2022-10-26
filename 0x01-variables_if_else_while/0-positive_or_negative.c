1667351e22d48c66fabd7d54838b3f58dbc6d94b
Emmanuel <olarewajuemmanuel4@gmail.com>
2022-10-26 06:33:12 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
-int mai()
+
+/**
+* main - check if a number is positive or negative
+* @n: imputed value
+* Return: always 0 (success)
+*/
+
+int main()
+
 {
+
 int n;
 
 if (n > 0)
 {
-printf("%d is positive", n);
+printf( "%d is positive", n);
 }
 
 else
 {
-printf("%d is negative, n);
+printf("%d is negative", n);
 }
+
 return (0);
+
 }
