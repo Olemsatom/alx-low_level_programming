4ddf6d7d0ac7861236c3667e82fa0ffb3dca6e19
Emmanuel <olarewajuemmanuel4@gmail.com>
2022-10-27 15:04:37 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - print a to z in reverse form
+*
+* Return: always 0 (success)
+*/
+
+
+int main(void)
+
+
+{
+
+
+char letter;
+
+for (letter = 'z'; letter >= 'a';)
+
+{
+
+putchar(letter);
+
+letter–;
+}
+
+putchar('\n');
+
+return (0);
+}
