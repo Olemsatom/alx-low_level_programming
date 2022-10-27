f1ee894499e56a6d4a4c89ef32b06cba95a9e4cf
Emmanuel <olarewajuemmanuel4@gmail.com>
2022-10-27 15:28:29 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+* main - print base 16 digits starting from 0
+*
+* Return: always 0 (success)
+*/
+
+
+int main(void)
+
+
+{
+
+int n;
+char letter;
+
+
+n = 0;
+
+while(n < 10)
+{
+
+putchar((n % 10) + '0')
+
+}
+
+
+for (letter = 'a'; letter <= 'f';)
+
+{
+
+putchar(letter);
+letter++;
+
+}
+
+return (0);
+
+}
