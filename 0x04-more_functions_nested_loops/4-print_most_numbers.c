b42ae0b100d1cef5c14662f55876994904308737
Emmanuel <olarewajuemmanuel4@gmail.com>
2022-11-02 07:55:52 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_most_numbers - check the code.
+* 
+* 
+* Return: Always 0.
+*/
+void print_most_numbers(void)
+{
+int digit;
+if (!((a == '2') || (a == '4')
+_putchar(digit);
+_putchar('\n);
+return (0);
+}
