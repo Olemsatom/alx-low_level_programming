c47ea02f8d8b449aa15250bf3d34a306ffe89a94
Emmanuel <olarewajuemmanuel4@gmail.com>
2022-11-02 06:49:08 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char c;
+if (c >= 'A' && c <= 'Z')
+{
+return (1)
+}
+else
+{
+return (0)
+}
+
+return (0);
+}
