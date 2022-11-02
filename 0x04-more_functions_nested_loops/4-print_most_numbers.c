abf6dc210f6f55733377844c38c7d39a2a77109e
Emmanuel <olarewajuemmanuel4@gmail.com>
2022-11-02 08:06:19 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
 * print_most_numbers - check the code.
-* 
-* 
+*
+*
 * Return: Always 0.
 */
 void print_most_numbers(void)
 {
 int a;
 if (!((a == '2') || (a == '4')))
-{
 _putchar(a);
 _putchar('\n');
 }
