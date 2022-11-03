1e58eb30f84934c3a0079e8f52b5dbde4a28466d
Emmanuel <olarewajuemmanuel4@gmail.com>
2022-11-03 11:38:44 -0700

    task 6

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,17 @@ while (j < 11)
 {
 for (a = 0; a <= 14; a++)
 {
+if (a <= 9)
+{
 _putchar('0' + a);
 }
-j++;
+else
+{
+_putchar('0' + a / 10);
+_putchar('0' + a % 10);
+}
 }
 _putchar('\n');
+j++;
+}
 }
