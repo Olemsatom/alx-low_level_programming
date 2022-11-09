569e09ffe57fed6021f78d165ffa9492928380d6
Emmanuel <olarewajuemmanuel4@gmail.com>
2022-11-09 13:51:50 -0800

    task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,17 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "holberton.h"
+
 /**
-* main - prints its name + \n
-* @argc: number of arguments typed
-* @argv: array pointing to arguements
-* Return: 0
+* main - function to print file name
+* @argc: count of the arguments supplied to the program and the
+* @argv: an array of pointers to the strings
+* Return: Always 0
 */
+
 int main(int argc, char *argv[])
 {
-argc = 0;
-
-printf("%s\n", argv[argc]);
+if (argc > 0)
+printf("%s\n", argv[0]);
 return (0);
 }
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,26 +1,13 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
-* main - Prints the number of arguments passed into main
-* @argc: Number of command line arguments
-* @argv: Array name
-* Return: 0
+* main - function
+* @argc: number of arguments passed
+* Return: Always 0
+* @argv: arguments passed here
 */
-
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-int i;
-
-if (argc == 1)
 printf("%d\n", argc - 1);
-else
-{
-for (i = 0; *argv; i++, argv++)
-			;
-
-printf("%d\n", i - 1);
-}
-
 return (0);
 }
