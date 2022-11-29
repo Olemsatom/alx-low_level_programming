735692201d7786f775bad0f074e27a671978b513
Emmanuel <olarewajuemmanuel4@gmail.com>
2022-11-29 14:12:38 -0800

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints all elements of a linked list
+ * @h: list
+ *
+ * Description: singly linked list node structure
+ * Return: the number of nodes printed
+ */
+ 
+size_t print_listint(const listint_t *h)
+
+{
+	int nodes = 0;
+	
+	while (h)
+	{
+		nodes++;
+		printf("%d", h->n);
+		h = h->next;
+		printf("\n");
+	}
+	return (nodes);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+How to use linked lists
+0x13. C - More singly linked lists
+Data structure and algorithm
