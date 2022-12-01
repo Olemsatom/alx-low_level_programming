182e165382be3cf0b4dac605c2e807d43e4ebf17
Emmanuel <olarewajuemmanuel4@gmail.com>
2022-12-01 12:38:56 -0800

    bitwise operation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,20 @@
-#include "main.h"
-#include <stddef.h>
-
 /**
- * binary_to_uint - convert binary to unsigned int
- * @b: binary
- * Return: unsigned int
- */
+*binary_to_uint - converts a binary number to an unsigned int
+*@b: pointing to a string of 0 and 1 chars
+*Return: the converted number, or 0 if
+*/
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int n = 0;
 
-	int len = 0, i;
-	unsigned int sum = 0;
-
-	if (b == NULL)
-		return (sum);
-
-	/* find string length */
-	while (b[len] != '\0')
-		len++;
-	len -= 1;
-
-	/* iterate string and if '1' then multiply by power of 2 */
-	/* get power of 2 via binary (e.g. 1<<2 = 100 in binary = 4) */
-	i = 0;
-	while (b[i])
+	if (!b)
+		return (0);
+	while (*b)
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return (sum);
-
-		if (b[i] == '1')
-			sum += (1 * (1 << len));
-		i++;
-		len--;
+		if (*b != '0' && *b != '1')
+			return (0);
+		n = n * 2 + *b - '0';
+		++b;
 	}
-	return (sum);
+	return (n);
 }
